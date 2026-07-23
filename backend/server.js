d8624cfd12ad80f367a7ccd1e0d1752d6eb4df06
sumit kumar <shreyashi1705@gmail.com>
2026-07-23 22:52:40 +0530

    Add PDF upload and text extraction

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,60 @@
 const express = require("express");
+const multer = require("multer");
+const fs = require("fs");
+const pdf = require("pdf-parse");
+const cors = require("cors");
 
 const app = express();
 
-const PORT = 5000;
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.send("Backend is running!");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+
+    cb(null, "uploads/");
+  },
+
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+const upload = multer({ storage });
+
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.post("/api/upload", upload.single("resume"), async (req, res) => {
+  try {
+    console.log("Resume Uploaded");
+
+    const dataBuffer = fs.readFileSync(req.file.path);
+
+    const pdfData = await pdf(dataBuffer);
+
+    console.log("----------------------");
+    console.log(pdfData.text);
+    console.log("----------------------");
+
+    res.json({
+      success: true,
+      filename: req.file.filename,
+      extractedText: pdfData.text,
+    });
+  } catch (err) {
+    console.log(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Error reading PDF",
+    });
+  }
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
 });
